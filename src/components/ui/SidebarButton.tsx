7fb9276cd34d9e91d9d305f6235b0bda85f97397
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-30 15:08:46 -0300

    Feat: Adicionando icones do lucide e prop End

--- src/components/ui/SidebarButton.tsx
+++ src/components/ui/SidebarButton.tsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import type { ReactNode } from 'react'; // Importe o tipo ReactNode
 
 interface SidebarButtonProps {
   page_name: string;
   to: string;
+  icon?: ReactNode;
+  end?: boolean;
 }
-
 
 const SidebarButton = (props: SidebarButtonProps) => {
   return (
-    <div className='flex w-full items-center radius'>
-        <NavLink to={props.to} className='p-2 text-start w-full hover:bg-gray-100 text-gray-600 font-normal rounded-sm' end>
-            {props.page_name}
+    <div className='flex w-full items-center'>
+        <NavLink 
+            to={props.to} 
+            end={props.end}
+            className={({ isActive }) => 
+                `flex items-center gap-3 p-2 text-start w-full rounded-md transition-colors duration-150 ${
+                    isActive 
+                    ? 'bg-blue-100 text-blue-600 font-semibold' 
+                    : 'text-gray-600 font-medium hover:bg-gray-100'
+                }`
+            }
+        >
+            {props.icon}
+            <span>{props.page_name}</span>
         </NavLink>
     </div>
-  )
-}
+  );
+};
 
 export default SidebarButton
