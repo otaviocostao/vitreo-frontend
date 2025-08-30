c771163ad268167650f5b1824c6e274796cb9867
Otavio <dev.otaviocostao@gmail.com>
2025-08-30 14:16:45 -0300

    Feat: Componente para cabeçalho de cada página

---
+++ src/components/HeaderTitlePage.tsx
@@ -0,0 +1,15 @@
+
+interface HeaderTitleProps {
+  page_name: string
+}
+
+
+const HeaderTitlePage = (props: HeaderTitleProps) => {
+  return (
+    <div className='w-full h-fit box-border p-2 '>
+      <h1 className="text-2xl text-blue-600 font-semibold border-l-4 pl-2 m-1"> {props.page_name}</h1>
+    </div>
+  )
+}
+
+export default HeaderTitlePage
