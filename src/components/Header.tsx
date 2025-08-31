3083a24b6f1c058d514320e0ed8ee144b6a0911e
Otavio <dev.otaviocostao@gmail.com>
2025-08-31 13:26:46 -0300

    Fix: Corrigindo tamanho do h1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 function Header() {
   return (
     <div className="w-full flex box-border border-b-1 border-gray-200">
-        <h1 className='w-65 text-2xl font-bold text-blue-600 border-e-1 p-3 box-border border-gray-200'>Vitreo</h1>
+        <h1 className='w-60 text-2xl font-bold text-blue-600 border-e-1 p-3 box-border border-gray-200'>Vitreo</h1>
     </div>
   )
 }
