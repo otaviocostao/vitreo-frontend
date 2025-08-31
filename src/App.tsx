f6493b061ab3c4c92eead7b25ac9b5576d106d20
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-30 21:35:07 -0300

    Feat: Rota para a página de venda

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import NovaVenda from './pages/NovaVenda'
 
 function App() {
 
@@ -10,6 +11,10 @@ function App() {
         <Route 
               path="/" 
               element={<Home />}
+            />
+        <Route 
+              path="/vendas/nova" 
+              element={<NovaVenda />}
             />
       </Routes>
     </BrowserRouter>
