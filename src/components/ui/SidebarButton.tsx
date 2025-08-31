81086efb5b8422506003fe934ada34ddcc2c6acc
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-30 21:30:37 -0300

    Diminuindo tamanho da fonte do span

--- src/components/ui/SidebarButton.tsx
+++ src/components/ui/SidebarButton.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import type { ReactNode } from 'react'; // Importe o tipo ReactNode
+import type { ReactNode } from 'react';
 
 interface SidebarButtonProps {
   page_name: string;
@@ -15,7 +15,7 @@ const SidebarButton = (props: SidebarButtonProps) => {
             to={props.to} 
             end={props.end}
             className={({ isActive }) => 
-                `flex items-center gap-3 p-2 text-start w-full rounded-md transition-colors duration-150 ${
+                `flex items-center gap-3 p-2 text-sm text-start w-full rounded-md transition-colors duration-150 ${
                     isActive 
                     ? 'bg-blue-100 text-blue-600 font-semibold' 
                     : 'text-gray-600 font-medium hover:bg-gray-100'
