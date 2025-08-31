66156f37967409b8f62ddc237f96c4a2de5514b7
otaviocostao <dev.otaviocostao@gmail.com>
2025-08-30 21:33:56 -0300

    Feat: Componente de DateInput com label

---
+++ src/components/ui/DateInput.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface DateInputProps{
+    label: string
+}
+
+const DateInput = (props: DateInputProps) => {
+  return (
+    <div className="flex items-center justify-between gap-2">
+        <label className="font-semibold text-gray-600 text-sm" >{props.label}</label>
+        <input type="date" className='px-3 py-2 bg-white border border-gray-300 rounded-md
+            text-sm text-gray-700 placeholder-gray-400
+            focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500' />
+    </div>
+  )
+}
+
+export default DateInput
