ffd013785bdf13b9ac68cedf3c9de79ee4338e2e
otaviocostao <dev.otaviocostao@gmail.com>
2025-09-11 21:53:46 -0300

    Feat: Tabela de listagem de clientes

--- src/components/ClientsTable.tsx
+++ src/components/ClientsTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export interface Client {
   id: number;
@@ -15,41 +15,48 @@ interface ClientsTableProps {
 
 const ClientsTable: React.FC<ClientsTableProps> = ({ clients }) => {
   const tableHeaders = ['#', 'Cliente', 'Endereço', 'D/N', 'Telefone', ' '];
+    
+  const navigate = useNavigate();
+  const handleRowClick = ( clienteId: number) => {
+    navigate(`/clientes/${clienteId}`);
+  };
 
   return (
     <div className="bg-white border-1 border-gray-200 rounded-lg overflow-x-auto">
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
-          <tr>
-            {tableHeaders.map((header) => (
-              <th
-                key={header}
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                {header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
-          {clients.map((client) => (
-            <tr key={client.id} className="hover:bg-gray-50">
-              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{client.id}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{client.name}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{client.address}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{client.birthDate}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{client.phone}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                <Link to={`/clients/${client.id}`} className="text-blue-600 hover:text-blue-800">
-                  Ver mais
-                </Link>
-              </td>
+      <div className="max-h-[60vh] overflow-y-auto">
+        <table className="min-w-full divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+            <tr>
+                {tableHeaders.map((header) => (
+                <th
+                    key={header}
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >   
+                    {header}
+                </th>
+                ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+            {clients.map((client) => (
+                <tr key={client.id} className="hover:bg-gray-50 cursor-pointer" onClick={() => handleRowClick(client.id)}>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{client.id}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{client.name}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{client.address}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{client.birthDate}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{client.phone}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                    <Link to={`/clients/${client.id}`} className="text-blue-600 hover:text-blue-800">
+                    Ver mais
+                    </Link>
+                </td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+        </div>
+      </div>
   );
 };
 
