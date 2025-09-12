e85887348314e4a94f84b0bb10739ba1ccc74fca
otaviocostao <dev.otaviocostao@gmail.com>
2025-09-11 21:52:54 -0300

    Fix: Corrigindo componente de título da página

--- src/components/HeaderTitlePage.tsx
+++ src/components/HeaderTitlePage.tsx
@@ -3,10 +3,9 @@ interface HeaderTitleProps {
   page_name: string
 }
 
-
 const HeaderTitlePage = (props: HeaderTitleProps) => {
   return (
-    <div className='w-full h-fit box-border p-2 '>
+    <div className='w-fit h-fit box-border p-4 '>
       <h1 className="text-2xl text-blue-600 font-semibold border-l-4 pl-2 m-1"> {props.page_name}</h1>
     </div>
   )
