55929b75d35bd9c1482dfd94ef4c52b6d7f63e1f
otaviocostao <dev.otaviocostao@gmail.com>
2025-09-12 23:28:13 -0300

    Fix: Removendo border-top

--- src/components/SaveCancelButtonsArea.tsx
+++ src/components/SaveCancelButtonsArea.tsx
@@ -7,7 +7,7 @@ interface ButtonsAreaProps {
 
 const SaveCancelButtonsArea = (props: ButtonsAreaProps) => {
   return (
-    <div className="flex justify-end items-center gap-4 mt-8 pt-6 border-t border-gray-200">
+    <div className="flex justify-end items-center gap-4 py-3">
         <Button type="button" variant="secondary">
             {props.textButton1}
         </Button>
