a9e4f1bb7f91b0d7d09beb58d601f7fa66534957
otaviocostao <dev.otaviocostao@gmail.com>
2025-09-12 23:29:57 -0300

    Feat: Input para preço

--- src/components/ui/PriceInput.tsx
+++ src/components/ui/PriceInput.tsx
@@ -1,24 +1,29 @@
-interface PriceInputProps{
-    label: string,
-    placeholder?: string
+import React from 'react';
+import { cn } from '../../lib/utils'; 
+interface PriceInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  id: string;
+  containerClassName?: string;
 }
 
-const PriceInput = (props:PriceInputProps) => {
+const PriceInput: React.FC<PriceInputProps> = ({ label, id, containerClassName, ...rest }) => {
   return (
-    <div className="flex items-center justify-between gap-2">
-        <label className="font-normal text-gray-700 text-sm">{props.label}</label>
-
-        <input
-            type="text"
-            placeholder={props.placeholder}
-            className="
-                block w-28 px-2 py-1.5 bg-white border border-gray-300 rounded-md
-                text-sm text-gray-700 placeholder-gray-400
-                focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500
-                "
-            />
+    <div className={cn("flex justify-between items-center", containerClassName)}>
+      <label htmlFor={id} className="text-sm font-medium text-gray-700">
+        {label}
+      </label>
+      <input
+        id={id}
+        className={cn(
+          'block w-28 px-3 py-1.5 bg-white border border-gray-300 rounded-md',
+          'placeholder-gray-400 text-sm',
+          'focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500',
+          'transition duration-150 ease-in-out'
+        )}
+        {...rest}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default PriceInput
+export default PriceInput;
