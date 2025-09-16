5aad0aaf43c709967661247d5e9398d21c9a73b2
Otavio <dev.otaviocostao@gmail.com>
2025-09-16 16:24:06 -0300

    Feat: adicionando rota para o botão de cancelar

--- src/components/SaveCancelButtonsArea.tsx
+++ src/components/SaveCancelButtonsArea.tsx
@@ -1,16 +1,20 @@
+import { NavLink } from "react-router-dom";
 import Button from "./ui/Button"
 
 interface ButtonsAreaProps {
   textButton1?: string;
   textButton2?: string;
+  cancelButtonPath?: string;
 }
 
 const SaveCancelButtonsArea = (props: ButtonsAreaProps) => {
   return (
     <div className="flex justify-end items-center gap-4 py-3">
-        <Button type="button" variant="secondary">
-            {props.textButton1}
-        </Button>
+        <NavLink to={{ pathname: props.cancelButtonPath}}>
+          <Button type="button" variant="secondary">
+              {props.textButton1}
+          </Button>
+        </NavLink>
         <Button type="submit" variant="primary">
             {props.textButton2}
         </Button>
