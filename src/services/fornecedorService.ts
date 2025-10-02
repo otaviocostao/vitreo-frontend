ec0447f6aea5ddaf98fe066abb640c79b1282576
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-02 19:42:34 -0300

    Feat: Método para buscar fornecedores no backend

--- src/services/fornecedorService.ts
+++ src/services/fornecedorService.ts
@@ -1,7 +1,24 @@
+import type { FornecedorResponse } from '../types/fornecedor';
 import type { Page } from '../types/pagination';
 import type { FornecedorOption } from '../types/produto';
 
 import api from './api';
+
+export const getFornecedores = async (page: number, size: number): Promise<Page< FornecedorResponse>> => {
+  try {
+    const response = await api.get<Page<FornecedorResponse>>('/fornecedores', {
+      params: {
+        page: page - 1,
+        size: size,
+        sort: 'razaoSocial,asc'
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar fornecedores:", error);
+    throw error;
+  }
+};
 
 export const getFornecedoresOptions = async (): Promise<FornecedorOption[]> => {
   try {
