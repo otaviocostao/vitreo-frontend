de31ad866e9f31db7aa7b6f83a9715faa8d20eb8
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-02 23:17:46 -0300

    Feat: método createFornecedor

--- src/services/fornecedorService.ts
+++ src/services/fornecedorService.ts
@@ -1,8 +1,18 @@
-import type { FornecedorResponse } from '../types/fornecedor';
+import type { FornecedorPayload, FornecedorResponse } from '../types/fornecedor';
 import type { Page } from '../types/pagination';
 import type { FornecedorOption } from '../types/produto';
 
 import api from './api';
+
+export const createFornecedor = async (fornecedorData: FornecedorPayload) => {
+  try {
+    const response = await api.post('/fornecedores', fornecedorData);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao cadastrar fornecedor:", error);
+    throw error;
+  }
+};
 
 export const getFornecedores = async (page: number, size: number): Promise<Page< FornecedorResponse>> => {
   try {
@@ -28,6 +38,26 @@ export const getFornecedoresOptions = async (): Promise<FornecedorOption[]> => {
     return response.data.content;
   } catch (error) {
     console.error("Erro ao buscar fornecedores:", error);
+    throw error;
+  }
+};
+
+export const getFornecedorById = async (id: string): Promise<FornecedorResponse> => {
+  try {
+    const response = await api.get<FornecedorResponse>(`/fornecedores/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(`Erro ao buscar fornecedor com ID ${id}:`, error);
+    throw error;
+  }
+};
+
+export const updateFornecedor = async (id: string, data: FornecedorPayload): Promise<FornecedorResponse> => {
+  try {
+    const response = await api.put<FornecedorResponse>(`/fornecedores/${id}`, data);
+    return response.data;
+  } catch (error) {
+    console.error(`Erro ao atualizar fornecedor com ID ${id}:`, error);
     throw error;
   }
 };
