70faf730e8012a09e2f88e648d6453c478fc8aa9
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-15 14:45:59 -0300

    Feat Adicionando campos que faltavam para Cliente

--- src/types/cliente.ts
+++ src/types/cliente.ts
@@ -3,20 +3,32 @@ import type { EnderecoPayload, EnderecoResponse } from "./endereco";
 export interface ClientePayload {
   nome: string;
   sobrenome: string;
-  cpf: string;
-  dataNascimento: string;
+  cpf?: string;
+  rg?: string;
+  dataNascimento?: string;
+  genero?: string;
+  naturalidade?: string;
+  endereco?: EnderecoPayload;
+  telefone?: string;
+  telefoneSecundario?: string;
   email?: string;
-  telefone: string;
-  endereco: EnderecoPayload;
+  observacoes?: string;
 }
 
 export interface ClienteResponse {
   id: string;
   nomeCompleto: string;
+  nome: string;
+  sobrenome: string;
   cpf: string;
+  rg: string;
   dataNascimento: string;
+  genero: string;
+  naturalidade: string;
   email: string;
   telefone: string;
+  telefoneSecundario: string;
   dataCadastro: string;
   endereco: EnderecoResponse;
+  observacoes: string;
 }
