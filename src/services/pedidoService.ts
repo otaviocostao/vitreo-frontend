2d192173c6ebc7d4777222fd1d6fdb3464fcc6a5
otaviocostao <dev.otaviocostao@gmail.com>
2025-11-04 14:57:59 -0300

    Feat: Implementando um GET para puxar os pedidos do backend + Implementando filtros

--- src/services/pedidoService.ts
+++ src/services/pedidoService.ts
@@ -1,7 +1,28 @@
-import type { PedidoPayload } from '../types/pedido';
+import type { Page } from '../types/pagination';
+import type { PedidoPayload, PedidoResponse } from '../types/pedido';
 import api from './api';
+
+export interface PedidoFiltros {
+  page?: number;
+  size?: number;
+}
 
 export const createPedido = async (data: PedidoPayload) => {
     const response = await api.post('/pedidos', data);
     return response.data;
 }
+
+export const getPedidos = async (filtros: PedidoFiltros = {}): Promise<Page<PedidoResponse>> => {
+  try {
+    const response = await api.get<Page<PedidoResponse>>('/pedidos', {
+      params: {
+        page: filtros.page || 0,
+        size: filtros.size || 20,
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error(`Erro ao buscar pedidos:`, error);
+    throw error;
+  }
+};
