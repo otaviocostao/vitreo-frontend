b82c06a82095ba2780e6e09afe1acf3b471391fc
otaviocostao <dev.otaviocostao@gmail.com>
2025-11-11 11:33:20 -0300

    Fix: definindo cor do botão

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -16,7 +16,7 @@ const Button: React.FC<ButtonProps> = ({ children, variant = 'primary', classNam
     secondary: "px-4 py-2 bg-white text-gray-700 border border-gray-300 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-blue-500",
     destructive: "px-4 py-2 bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-red-500",
     smallDelete: 
-    'px-2 py-2 bg-none bg-input/30 border-none hover:text-red-500 hover:bg-gray-100',
+    'px-2 py-2 bg-none bg-input/30 text-gray-700 border-none hover:text-red-500 hover:bg-gray-100',
   };
 
   return (
