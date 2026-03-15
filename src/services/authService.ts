8a2dcf1cfb10191169ca8a4d1d469cc32da39f83
otaviocostao <dev.otaviocostao@gmail.com>
2026-03-14 23:05:17 -0300

    feat: implementando o método de login na authService

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,4 +1,18 @@
+import type { LoginResponse } from "../types/login";
 import api from "./api";
+
+export const handleLogin = async (userData: any) => {
+    try {
+        const response = await api.post<LoginResponse>("/auth/login", userData);
+
+        localStorage.setItem("token", response.data.jwtToken);
+        localStorage.setItem("userEmail", response.data.email);
+
+        window.location.href = "/"; 
+    } catch (err: any) {
+        throw err;
+    }
+};
 
 export const handleLogout = async () => {
     try {
