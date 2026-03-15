d7c122b54912971a6c0b62ce193da6b9a2e22ad8
otaviocostao <dev.otaviocostao@gmail.com>
2026-03-14 22:55:04 -0300

    feat: implementando metodo onClick nos botões da sidebar

--- src/components/ui/SidebarButton.tsx
+++ src/components/ui/SidebarButton.tsx
@@ -3,30 +3,41 @@ import type { ReactNode } from 'react';
 
 interface SidebarButtonProps {
   page_name: string;
-  to: string;
+  to?: string;
+  onClick?: () => void; 
   icon?: ReactNode;
   end?: boolean;
 }
 
 const SidebarButton = (props: SidebarButtonProps) => {
+  const baseClass = "flex items-center gap-3 p-2 text-sm text-start w-full rounded-md transition-colors duration-300 cursor-pointer";
+  const inactiveClass = "text-gray-600 font-medium hover:bg-gray-100";
+  const activeClass = "bg-blue-100 text-blue-600 font-semibold";
+
   return (
     <div className='flex w-full items-center'>
+      {props.to ? (
         <NavLink 
             to={props.to} 
             end={props.end}
             className={({ isActive }) => 
-                `flex items-center gap-3 p-2 text-sm text-start w-full rounded-md transition-colors duration-300 ${
-                    isActive 
-                    ? 'bg-blue-100 text-blue-600 font-semibold' 
-                    : 'text-gray-600 font-medium hover:bg-gray-100'
-                }`
+                `${baseClass} ${isActive ? activeClass : inactiveClass}`
             }
         >
             {props.icon}
             <span>{props.page_name}</span>
         </NavLink>
+      ) : (
+        <button 
+            onClick={props.onClick}
+            className={`${baseClass} ${inactiveClass}`}
+        >
+            {props.icon}
+            <span>{props.page_name}</span>
+        </button>
+      )}
     </div>
   );
 };
 
-export default SidebarButton
+export default SidebarButton;
